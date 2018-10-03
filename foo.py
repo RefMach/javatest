11f7d70634e3e47db52e4528ea13e4baac31a797
RefMach <kakagut@spindl-e.com>
2018-10-03 14:46:44 +0300

    "init"

--- foo.py
+++ foo.py
@@ -1,15 +1,36 @@
-state = [['x', 'o', 'x'], ['x', 'x', 'x'], ['x', 'x', 'o']]
-
-state[1][0] = 'x'
-
-for row in state:
-    print(row)
+def compute_score(y, y_hat):  # to be minimized
+    assert len(y) == len(y_hat)
+    return -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
 
 
-def is_winning_row(row, char):
-    return all([a == char for a in row])
+def predict_baseline(X_train, y_train, X_test):
+    return np.array([y_train.mean()] * len(X_test))
 
 
-for row in state:
-    if is_winning_row(row, 'x') or is_winning_row(row, 'o'):
-        print('Win')
+def full_cv(predict_func, X, y, folds=10, shuffle=True, verbose=True):
+    y = np.array(y)
+    index_shuf = np.array(range(len(X)))
+    if shuffle:
+        random.shuffle(index_shuf)
+
+    X_folds = np.array_split(index_shuf, folds)
+    y_folds = np.array_split(index_shuf, folds)
+    y_predict = np.array([])
+    for k in range(folds):
+        if verbose:
+            print('\r{:7.5}%  '.format(k * 100.0 / folds))
+        # We use 'list' to copy, in order to 'pop' later on
+        X_train = list(X_folds)
+        X_test = X_train.pop(k)
+        X_train = np.concatenate(X_train)
+        y_train = list(y_folds)
+        y_train.pop(k)
+        y_train = np.concatenate(y_train)
+        new_y_hat = predict_func(X.ix[X_train], y[y_train], X.ix[X_test])
+        y_predict = np.concatenate([y_predict, new_y_hat])
+    return compute_score(y[index_shuf], y_predict)
+
+
+baseline_score = full_cv(predict_baseline, adult, adult.y, verbose=False)
+print('baseline_score=', baseline_score)  # 0.568739686466 (10 folds)
+# baseline_score = compute_score(adult.income, np.array([adult.income.mean()]*len(adult.income))) # 0.5520112931
