9da09ea822833ed750ecb79dc19d50832fe3bfa8
RefMach <kakagut@spindl-e.com>
2018-10-03 14:10:28 +0300

    formatting

--- foo.py
+++ foo.py
@@ -1,12 +1,10 @@
-state = [['x','o','x'], ['x','x','x'],['x','x','o']]
+state = [['x', 'o', 'x'], ['x', 'x', 'x'], ['x', 'x', 'o']]
 
 state[1][0] = 'x'
-
 
 for row in state:
     print(row)
 
 for row in state:
-    if all([a=='x' for a in row]) or all([a=='o' for a in row]):
+    if all([a == 'x' for a in row]) or all([a == 'o' for a in row]):
         print('Win')
-
